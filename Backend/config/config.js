3b9523936c86f059b196ae5caf5dec306ef1f910
Jason Guo <jhg294@cornell.edu>
2026-01-28 14:38:58 -0500

    unused user routes and schemas deleted

--- Backend/config/config.js
+++ Backend/config/config.js
@@ -3,7 +3,6 @@ require('dotenv').config();
 module.exports = {
   port: process.env.PORT || 8000,
   mongodbUri: process.env.MONGO_URI,
-  jwtSecret: process.env.JWT_SECRET,
   openaiApiKey: process.env.OPENAI_API_KEY,
   langChainApiKey: process.env.LANGCHAIN_API_KEY
 };
--- Backend/routes/index.js
+++ Backend/routes/index.js
@@ -1,5 +1,4 @@
 const express = require("express");
-const userRoutes = require("./user");
 const openAIRoutes = require("./openai");
 const codeRoutes = require("./code");
 const ragRoutes = require("./rag");
@@ -7,7 +6,6 @@ const ragRoutes = require("./rag");
 
 const router = express.Router()
 
-router.use("/user", userRoutes);
 router.use("/openai", openAIRoutes);
 router.use("/code", codeRoutes);
 router.use("/rag", ragRoutes);
