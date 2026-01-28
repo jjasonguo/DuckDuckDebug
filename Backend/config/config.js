c5748477e555b283bc863f0edf50375dd9840df3
Jason Guo <jhg294@cornell.edu>
2026-01-28 14:20:50 -0500

    mistral routes never used

--- Backend/config/config.js
+++ Backend/config/config.js
@@ -5,7 +5,5 @@ module.exports = {
   mongodbUri: process.env.MONGO_URI,
   jwtSecret: process.env.JWT_SECRET,
   openaiApiKey: process.env.OPENAI_API_KEY,
-  langChainApiKey: process.env.LANGCHAIN_API_KEY,
-  mistralApiKey: process.env.MISTRAL_API_KEY,
-  kimMistralApiKey: process.env.KIM_MISTRAL_API_KEY
+  langChainApiKey: process.env.LANGCHAIN_API_KEY
 };
--- Backend/routes/index.js
+++ Backend/routes/index.js
@@ -1,6 +1,5 @@
 const express = require("express");
 const userRoutes = require("./user");
-const mistralRoutes = require("./mistral");
 const openAIRoutes = require("./openai");
 const codeRoutes = require("./code");
 const ragRoutes = require("./rag");
@@ -9,7 +8,6 @@ const ragRoutes = require("./rag");
 const router = express.Router()
 
 router.use("/user", userRoutes);
-router.use("/mistral", mistralRoutes);
 router.use("/openai", openAIRoutes);
 router.use("/code", codeRoutes);
 router.use("/rag", ragRoutes);
