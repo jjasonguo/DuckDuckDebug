e52a64942d4117cff6b74bfbe96bedee1309c8d2
Jason Guo <jhg294@cornell.edu>
2026-01-28 23:45:02 -0500

    clear db on init

--- Backend/server.js
+++ Backend/server.js
@@ -11,14 +11,15 @@ const port = config.port;
 app.use(cors())
 app.use(express.json());
 
-mongoose.connect(config.mongodbUri)
-  .then(() => console.log('Connected to MongoDB'))
-  .catch((err) => console.log('Failed to connect to MongoDB:', err));
-
-
 // mongoose.connect(config.mongodbUri)
-//   .then(async () => {await Code.deleteMany({}); console.log('Connected to MongoDB');})
+//   .then(() => console.log('Connected to MongoDB'))
 //   .catch((err) => console.log('Failed to connect to MongoDB:', err));
+
+
+// Would delete all previous data in the database
+mongoose.connect(config.mongodbUri)
+  .then(async () => {await Code.deleteMany({}); console.log('Connected to MongoDB');})
+  .catch((err) => console.log('Failed to connect to MongoDB:', err));
 
 app.use('/api', routes);
 
